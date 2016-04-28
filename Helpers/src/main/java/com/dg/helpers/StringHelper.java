d3702a2c4790ffd237702b9545ec3781236e23dd
Daniel Cohen Gindi <danielgindi@gmail.com>
2016-04-28 15:24:19 +0300

    Improved default formatting of Decimals with StringHelper

--- Helpers/src/main/java/com/dg/helpers/StringHelper.java
+++ Helpers/src/main/java/com/dg/helpers/StringHelper.java
@@ -1,5 +1,8 @@
 package com.dg.helpers;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 import java.util.regex.Pattern;
 
 /**
@@ -11,6 +14,28 @@ public class StringHelper
     {
         if (value == null) return null;
         else if (value instanceof String) return (String)value;
+        else if (value instanceof Double)
+        {
+            if ((Double)value == (long)value)
+            {
+                return ((Long)(long)value).toString();
+            }
+            else
+            {
+                return getCleanDecimalFormatter().format((double)value);
+            }
+        }
+        else if (value instanceof Float)
+        {
+            if ((Float)value == (long)value)
+            {
+                return ((Long)(long)value).toString();
+            }
+            else
+            {
+                return getCleanDecimalFormatter().format((float)value);
+            }
+        }
         else return value.toString();
     }
 
@@ -18,7 +43,42 @@ public class StringHelper
     {
         if (value == null) return defaultValue;
         else if (value instanceof String) return (String)value;
+        else if (value instanceof Double)
+        {
+            if ((Double)value == (long)value)
+            {
+                return ((Long)(long)value).toString();
+            }
+            else
+            {
+                return getCleanDecimalFormatter().format((double)value);
+            }
+        }
+        else if (value instanceof Float)
+        {
+            if ((Float)value == (long)value)
+            {
+                return ((Long)(long)value).toString();
+            }
+            else
+            {
+                return getCleanDecimalFormatter().format((float)value);
+            }
+        }
         else return value.toString();
+    }
+
+    private static DecimalFormat sCleanDecimalFormatter;
+
+    private static DecimalFormat getCleanDecimalFormatter()
+    {
+        if (sCleanDecimalFormatter == null)
+        {
+            DecimalFormat df = new DecimalFormat("0", DecimalFormatSymbols.getInstance(Locale.getDefault()));
+            df.setMaximumFractionDigits(340);
+            sCleanDecimalFormatter = df;
+        }
+        return sCleanDecimalFormatter;
     }
 
     private static Pattern pattern = Pattern.compile("^[a-zA-Z0-9\\._%+\\-]+@[a-zA-Z0-9\\-]+(\\.[a-zA-Z0-9\\-]+)*$");
