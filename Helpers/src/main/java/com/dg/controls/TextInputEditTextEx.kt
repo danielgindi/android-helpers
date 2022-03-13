faba889b0605d90ad9e64fc85481d0b653e2fea8
Daniel Cohen Gindi <danielgindi@gmail.com>
2022-03-13 12:55:43 +0200

    [feature] added TextInputEditTextEx to complement TextInputLayoutEx

---
+++ Helpers/src/main/java/com/dg/controls/TextInputEditTextEx.kt
@@ -0,0 +1,74 @@
+package com.dg.controls
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.util.AttributeSet
+import com.dg.R
+import com.dg.helpers.FontHelper
+import com.google.android.material.textfield.TextInputEditText
+
+@Suppress("unused")
+class TextInputEditTextEx : TextInputEditText
+{
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    {
+        setCustomFontFamily(context, attrs)
+    }
+
+    constructor(context: Context,
+                attrs: AttributeSet,
+                defStyle: Int) : super(context, attrs, defStyle)
+    {
+        setCustomFontFamily(context, attrs)
+    }
+
+    private fun setCustomFontFamily(context: Context, attrs: AttributeSet)
+    {
+        if (isInEditMode)
+        {
+            return
+        }
+
+        val fontFamily: String?
+        var styledAttributes: TypedArray? = null
+        try
+        {
+            styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.TextInputEditTextEx)
+            fontFamily = styledAttributes!!.getString(R.styleable.TextInputEditTextEx_customFontFamily)
+        }
+        finally
+        {
+            styledAttributes?.recycle()
+        }
+
+        if (fontFamily != null && !fontFamily.isEmpty())
+        {
+            paintFlags = this.paintFlags or Paint.SUBPIXEL_TEXT_FLAG or Paint.LINEAR_TEXT_FLAG
+            setCustomFont(fontFamily)
+        }
+    }
+
+    @Deprecated("This version was a mistake", replaceWith = ReplaceWith("setCustomFont(fontFamily)"))
+    fun setCustomFont(context: Context, fontFamily: String): Boolean
+    {
+        return setCustomFont(fontFamily)
+    }
+
+    fun setCustomFont(fontFamily: String): Boolean
+    {
+        val typeface: Typeface? = FontHelper.getFont(context, fontFamily)
+        return if (typeface != null)
+        {
+            setTypeface(typeface)
+            true
+        }
+        else
+        {
+            false
+        }
+    }
+}
